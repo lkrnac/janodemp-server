7b552313178e6462e0bddcf0faabfdd96530f2e5
lubos krnac <lubos.krnac@gmail.com>
2014-11-23 22:47:00 +0000

    Gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,33 @@ var paths = {
     ]
 };
 
+var errorOccured = false;
+/**
+ * This task is here to exit from process with error code.
+ * This way CI server knows that process failed.
+ * It is needed because gulp-plumber forces 0 error code
+ * even when error occurs.
+ */
+gulp.task('checkError', ['test'], function () {
+  if (errorOccured) {
+    console.log('Err, distor occured, exitting build process... ');
+    process.exit(1);
+  }
+});
+
+/**
+ * Logs error into variable
+ */
+var errorHandler = function () {
+  console.log('Error occured... ');
+  errorOccured = true;
+};
+
 gulp.task('build', function () {
   gulp.src([paths.server, paths.serverTests])
+    .pipe(plugins.plumber({
+      errorHandler: errorHandler
+    }))
     .pipe(plugins.jshint())
     .pipe(plugins.jshint.reporter(stylish))
     .pipe(plugins.jshint.reporter('fail'));
@@ -24,6 +49,10 @@ gulp.task('build', function () {
 
 gulp.task('test', ['build'], function () {
 
+});
+
+gulp.task('watch', function () {
+  gulp.watch([paths.server, paths.serverTests], ['test']);
 });
 
 gulp.task('clean', function (callback) {
