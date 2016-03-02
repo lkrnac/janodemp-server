544c6bfa65d5ac13aeb2a125236a9bc0a4255592
Lubos Krnac <lubos.krnac@gmail.com>
2016-03-02 20:09:09 +0000

    Create admin user only if doesn't exist

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,16 +1,28 @@
 "use strict";
 /* eslint-disable no-console */
 
+var printCreatedUsers = function (err, users) {
+  if (err) {
+    throw err;
+  }
+
+  console.log("Created users:", users);
+};
+
+var users = [
+  {username: "admin", email: "admin@janodemp.net", password: "janodemp-default"}
+];
+
 module.exports = function (app) {
   var User = app.models.user;
 
-  User.create([
-    {username: "admin", email: "admin@janodemp.net", password: "janodemp-default"}
-  ], function (err, users) {
-    if (err) {
-      throw err;
+  User.exists(1, function (err1, exists) {
+    if (err1) {
+      throw err1;
     }
 
-    console.log("Created users:", users);
+    if (!exists) {
+      User.create(users, printCreatedUsers);
+    }
   });
 };
