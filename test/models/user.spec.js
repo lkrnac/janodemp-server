54b3f1ac4c9f088fec85e564c62385b0c7b91164
Lubos Krnac <lubos.krnac@gmail.com>
2016-12-17 16:08:24 +0530

    Extract authentication test helper

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -27,51 +27,30 @@ describe('/users endpoint', () => {
   });
 
   it('should allow read users after login', (done) => {
-    server.json('post', '/api/users/login')
-      .send({
-        username: 'admin',
-        password: 'janodemp-default'
-      })
-      .expect(200, (error, response) => {
-        assert(typeof response.body === 'object');
-        const accessToken = response.body.id;
-        assert(accessToken, 'must have an access token');
-        assert.equal(response.body.userId, 1);
-
-        server.json('get', `/api/users?access_token=${accessToken}`)
-          .send()
-          .expect(200, (error2, response2) => {
-            assert(typeof response2.body === 'object');
-            assert(response2.body[0].id, 1);
-            assert(response2.body[0].username, 'admin');
-            done();
-          });
-      });
+    server.authenticated((accessToken) => {
+      server.json('get', `/api/users?access_token=${accessToken}`)
+        .send()
+        .expect(200, (error2, response2) => {
+          assert(typeof response2.body === 'object');
+          assert(response2.body[0].id, 1);
+          assert(response2.body[0].username, 'admin');
+          done();
+        });
+    });
   });
 
   it('should create admin user at start if doesn\'t exist', (done) => {
-    server.json('post', '/api/users/login')
-      .send({
-        username: 'admin',
-        password: 'janodemp-default'
-      })
-      .expect(200, (error, response) => {
-        assert(typeof response.body === 'object');
-        const accessToken = response.body.id;
-        assert(accessToken, 'must have an access token');
-        assert.equal(response.body.userId, 1);
-
-        server.json('get', `/api/users?access_token=${accessToken}`)
-          .send()
-          .expect(200, (error2, response2) => {
-            assert(typeof response2.body === 'object');
-            assert(response2.body[0].id, 1);
-            assert(response2.body[0].username, 'admin');
-            done();
-          });
-      });
+    server.authenticated((accessToken) => {
+      server.json('get', `/api/users?access_token=${accessToken}`)
+        .send()
+        .expect(200, (error2, response2) => {
+          assert(typeof response2.body === 'object');
+          assert(response2.body[0].id, 1);
+          assert(response2.body[0].username, 'admin');
+          done();
+        });
+    });
   });
-
 });
 
 describe('Unexpected Usage', () => {
--- test/test-utils/server-test-helpter.js
+++ test/test-utils/server-test-helpter.js
@@ -1,5 +1,6 @@
 const app = require('../../server/server');
 const request = require('supertest');
+const assert = require('assert');
 
 const json = (verb, url) => {
   return request(app)[verb](url)
@@ -19,6 +20,23 @@ const stop = (done) => {
   done();
 };
 
+const authenticated = (action) => {
+  json('post', '/api/users/login')
+    .send({
+      username: 'admin',
+      password: 'janodemp-default'
+    })
+    .expect(200, (error, response) => {
+      assert(typeof response.body === 'object');
+      const accessToken = response.body.id;
+      assert(accessToken, 'must have an access token');
+      assert.equal(response.body.userId, 1);
+
+      action(accessToken);
+    });
+};
+
 exports.json = json;
 exports.start = start;
 exports.stop = stop;
+exports.authenticated = authenticated;
